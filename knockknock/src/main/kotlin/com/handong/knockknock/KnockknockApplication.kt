67506930c8399161f9706bdf297bb7ded6abfee3
ko-inseoklee <tjrkd222@gmail.com>
2022-11-22 21:35:17 +0900

    코틀린 프로젝트 생성

---
+++ knockknock/src/main/kotlin/com/handong/knockknock/KnockknockApplication.kt
@@ -0,0 +1,11 @@
+package com.handong.knockknock
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KnockknockApplication
+
+fun main(args: Array<String>) {
+	runApplication<KnockknockApplication>(*args)
+}
---
+++ knockknock/src/test/kotlin/com/handong/knockknock/KnockknockApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.handong.knockknock
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KnockknockApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
